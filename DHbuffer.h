9fb6b746f5b8162b888ca760b7e2b2d5113adc49
cf9452mb <79483028+cf9452mb@users.noreply.github.com>
2021-10-20 18:19:16 -0500

    Update DHbuffer.h

--- DHbuffer.h
+++ DHbuffer.h
@@ -1,7 +1,7 @@
-/--------------------------------------------------
+//--------------------------------------------------
 // DHbuffer.h
 // Class for a buffer which reads, writes, packs and unpacks data
-// Author: Anuja Modi
+// Author: Anuja Modi, Jordan Hebler
 //--------------------------------------------------
 // This header file includes a class DataHeaderBuffer
 //--------------------------------------------------
@@ -9,28 +9,36 @@
 #ifndef DHBUFFER_H
 #define DHBUFFER_H
 
+#include "dhObject.h"
+
 /**
  * @file DHbuffer.h
  * @brief This is the header file for the DataHeaderBuffer class
  *
  * @author Anuja Modi
+ * @author Jordan Hebler
  */
+
 
 class DataHeaderBuffer
 {   public:
 
-DataHeaderBuffer();
+    DataHeaderBuffer();
+    
+    int Pack(dhObject& object);
 
-int ReadHeader(istream &);
+    //int readHeader(std::ifstream& file);
 
-int WriteHeader(ostream &) const;
+    int writeHeader(std::fstream& file);
 
 
-std::string getBuffer();
+    void setFieldpairs(std::vector<field> pairs);
+    
+    void Clear();
 
 private:
-char Delim;         
-std::string Buffer;       // string to hold field values
+    std::vector<std::string> headerinfo;
+    std::vector<field> fieldpairs; // string to hold field values
 };
 
 #endif
