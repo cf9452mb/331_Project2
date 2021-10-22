5b892a5516c754a62a6e34156d36c5cdd4e0718c
cf9452mb <79483028+cf9452mb@users.noreply.github.com>
2021-10-21 20:39:43 -0500

    Added Write, Unpack methods, and helper functions

--- DHbuffer.cpp
+++ DHbuffer.cpp
@@ -11,6 +11,9 @@
 
 #include "DHbuffer.h"
 
+std::string parseLine(std::string line);
+std::vector<field> parseFields(std::string line);
+
 /**
  * @file DHbuffer.cpp
  * @brief This is the implementation file for the DataHeaderBuffer class
@@ -39,7 +42,45 @@ int DataHeaderBuffer::Pack(dhObject& object){
     
 }
 
-int DataHeaderBuffer::writeHeader(std::fstream& file){
+int DataHeaderBuffer::Unpack(dhObject& object){
+    object.setFiletype(headerinfo[0]);
+    object.setFileversion(stoi(headerinfo[1]));
+    object.setHeadersize(stoi(headerinfo[2]));
+    object.setSizeformat(headerinfo[3]);
+    object.setIndexfile(headerinfo[4]);
+    object.setRecordcount(stoi(headerinfo[5]));
+    object.setFieldcount(stoi(headerinfo[6]));
+    for (field i : fieldpairs) {
+        object.setFieldpairs(i.name, i.type);
+    }
+    object.setPrimekey(stoi(headerinfo[7]));
+    
+    return 1;
+    
+}
+
+int DataHeaderBuffer::readDataHeader(std::ifstream& file){
+    
+    file.seekg(0, std::ios::beg);
+    std::string line;
+    int count = 0;
+    while(count < 7){
+        getline(file, line);
+        headerinfo.push_back(parseLine(line));
+        count++;
+    }
+
+    getline(file, line);
+    this->fieldpairs = parseFields(parseLine(line));
+    
+    getline(file, line);
+    headerinfo.push_back(parseLine(line));
+    
+    return 1;
+    
+}
+
+int DataHeaderBuffer::writeDataHeader(std::fstream& file){
     
     file.seekp(0, std::ios::beg);
     file << "FSTYPE  = " << headerinfo[0] << std::endl;
@@ -55,7 +96,7 @@ int DataHeaderBuffer::writeHeader(std::fstream& file){
     }
     file << std::endl;
     file << "PRIM/KEY= " << headerinfo[7] << std::endl;
-    
+   
     return 1;
 }
 
@@ -66,4 +107,36 @@ void DataHeaderBuffer::setFieldpairs(std::vector<field> pairs){
 void DataHeaderBuffer::Clear(){
     this->headerinfo.clear();
     this->fieldpairs.clear();
+}
+
+//Parses the keywords in the header file
+std::string parseLine(std::string line){
+    std::string delim("= ");
+    size_t start = line.find(delim);
+    line.erase(0, start + delim.length());
+    return line;
+}
+
+//Takes the (name,type) pairs line and parses it
+std::vector<field> parseFields(std::string line){
+    std::vector<std::string> words;
+    std::vector<field> fields;
+    std::string delim(")");
+    std::string delim2(",");
+    size_t pos = 0;
+    while ((pos = line.find(delim)) != std::string::npos) {
+            words.push_back(line.substr(0, pos + 1));
+            line.erase(0, pos + delim.length());
+    }
+    for (std::string s : words) {
+        field pair;
+        size_t p = s.find(delim2);
+        pair.name = s.substr(1, p - delim2.length());
+        s.erase(0, p + delim2.length());
+        p = s.find(delim);
+        pair.type = s.substr(0, p);
+        fields.push_back(pair);
+    }
+    
+    return fields;
 }
