f676dee7d6e2c355c607eba5dfc11c011252bcb8
cf9452mb <79483028+cf9452mb@users.noreply.github.com>
2021-10-21 20:36:58 -0500

    Update DHbuffer.h

--- DHbuffer.h
+++ DHbuffer.h
@@ -26,19 +26,20 @@ class DataHeaderBuffer
     DataHeaderBuffer();
     
     int Pack(dhObject& object);
+    
+    int Unpack(dhObject& object);
 
-    //int readHeader(std::ifstream& file);
+    int readDataHeader(std::ifstream& file);
 
-    int writeHeader(std::fstream& file);
-
+    int writeDataHeader(std::fstream& file);
 
     void setFieldpairs(std::vector<field> pairs);
     
     void Clear();
 
 private:
-    std::vector<std::string> headerinfo;
-    std::vector<field> fieldpairs; // string to hold field values
+    std::vector<std::string> headerinfo; // vector of strings to hold the header information
+    std::vector<field> fieldpairs; // vector of field structures to hold each fields (name,type) pairs
 };
 
 #endif
